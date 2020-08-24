a07aa0d203be00ff02c7f38f9dc344a5a06a8126
Jay <jeremy.davis@sonarsource.com>
2020-08-24 15:15:52 +0200

    Update main.js
    One for me!

--- static/main.js
+++ static/main.js
@@ -4,7 +4,7 @@ function render() {
     { name: "Siegfried", points: "dp" },
     { name: "Greg", points: "ddd" },
     { name: "Philippe", points: "d" },
-    { name: "Jay", points: "" },
+    { name: "Jay", points: "d" },
     { name: "Pierre", points: "u" },
     { name: "Xav", points: "p" },
     { name: "Laura", points: "d" },
